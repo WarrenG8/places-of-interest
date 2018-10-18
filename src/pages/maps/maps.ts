9d610014e549081f90f9c77684e763e30fa5ab9e
WarrenG8 <warrengyoung8@gmail.com>
2018-10-18 15:21:16 -0700

    Moved map logic to Map Provider

--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -2,7 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { UserProvider } from '../../providers/user/user';
 import { MapProvider } from '../../providers/map/map';
-import { LoginPage } from '../login/login';
+// import { LoginPage } from '../login/login';
 // import { Geolocation } from 'ionic-native';
 
 /**
@@ -11,7 +11,7 @@ import { LoginPage } from '../login/login';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-declare var google;
+
 
 @IonicPage()
 @Component({
@@ -20,41 +20,11 @@ declare var google;
 })
 export class MapsPage {
 
-  // @ViewChild('map') mapElement: ElementRef;
- 
-  map: any;
-  // mapInitialised: boolean = false;
-  // apiKey: any;
-
   constructor(public navCtrl: NavController, public navParams: NavParams, public _use: UserProvider, public _map: MapProvider) {
-    this.loadMap();
+    this._map.loadMap();
   }
 
-  // ionViewDidLoad(){
-  //   this.loadMap();
-  // }
- 
-  loadMap(){
- 
-    let locationOptions = {timeout: 20000, enableHighAccuracy: true};
- 
-    navigator.geolocation.getCurrentPosition(
- 
-        (position) => {
- 
-            let options = {
-              center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-              zoom: 16,
-              mapTypeId: google.maps.MapTypeId.ROADMAP
-            }
- 
-            this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
-        },
- 
-        (error) => {
-            console.log(error);
-        }, locationOptions
-    );
-  }
+  
 }
+
 
--- src/providers/map/map.ts
+++ src/providers/map/map.ts
@@ -7,13 +7,39 @@ import { Injectable } from '@angular/core';
   See https://angular.io/guide/dependency-injection for more info on providers
   and Angular DI.
 */
+declare var google;
+
 @Injectable()
 export class MapProvider {
 
-  url = '"https://maps.googleapis.com/maps/api/js?key=AIzaSyDHnwasWv7pFx7mWbH5098g8I367NLkbdc&callback=initMap"'
+  map: any;
+  // url = '"https://maps.googleapis.com/maps/api/js?key=AIzaSyDHnwasWv7pFx7mWbH5098g8I367NLkbdc&callback=initMap"'
 
   constructor(public http: HttpClient) {
     console.log('Hello MapProvider Provider');
+  }
+
+  loadMap(){
+ 
+    let locationOptions = {timeout: 20000, enableHighAccuracy: true};
+ 
+    navigator.geolocation.getCurrentPosition(
+ 
+        (position) => {
+ 
+            let options = {
+              center: new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
+              zoom: 16,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+ 
+            this.map = new google.maps.Map(document.getElementById("map_canvas"), options);
+        },
+ 
+        (error) => {
+            console.log(error);
+        }, locationOptions
+    );
   }
 
 }
