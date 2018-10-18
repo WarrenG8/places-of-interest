48e056dad19352b317507ea279dd60efd93db949
WarrenG8 <warrengyoung8@gmail.com>
2018-10-17 18:36:37 -0700

    Maps in construction

--- src/providers/map/map.ts
+++ src/providers/map/map.ts
@@ -10,6 +10,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MapProvider {
 
+  url = '"https://maps.googleapis.com/maps/api/js?key=AIzaSyDHnwasWv7pFx7mWbH5098g8I367NLkbdc&callback=initMap"'
+
   constructor(public http: HttpClient) {
     console.log('Hello MapProvider Provider');
   }
